bde3e9b8e947ace32d5f182d40ea82e1e74ac5c5
Eric Kimball <Eric-k93@live.com>
2017-11-30 12:20:20 -0800

    Add files via upload

--- onetimepad.java
+++ onetimepad.java
@@ -2,55 +2,43 @@ import java.util.Scanner;
 import java.lang.Character;
 public class onetimepad {
 
-
-
     public static int charToInt(char l) {
-		return(int)l;
+		int ascii = (int) character;
+		return ascii;
     }
 
     public static char intToChar(int i) {
-		
-    	return(char)i;
+		char result = Character.toString(i);
+		return result;
     }
 
+	public static String xor(String string, String key) {
+		char charString[] = string.toCharArray();
+		char charKey[] = key.toCharArray();
+		char results[];
+		int char1, char2, result;
+		char enciphered;
+		
+		for (int i = 0; i < charString.length; i++) {
+			char1 = charToInt(charString[i]);
+			char2 = charToInt(charKey[i]);
+			
+			result = char1 ^ char2;
+			enciphered = intToChar(result);
+			
+			val = Integer.toBinary(charString[i]);
+			val2 = Integer.toBinary(charKey[i]);			
+			xorResult = val ^ val2;
+		}
+	}
+	
     public static String encipher(String original, String onetimepad) {
-        int char1;
-        int char2;
-        char encodedChar;
-        int combinedVal;
-        String returnString = "";
-
-        for (int i = 0; i < original.length(); i++){
-            char1 = charToInt(original.charAt(i));
-            char2 = charToInt(onetimepad.charAt(i));
-            combinedVal = char1 + char2;
-            encodedChar = intToChar(combinedVal);
-            returnString += String.valueOf(encodedChar);
-            System.out.println(returnString);
-        }
-
-        return returnString;
-        system.out.println("made it out");
-
+		
+		
     }
 
 	public static String decipher(String cryptText, String onetimepad) {
-        /*int textChar;
-        int cipherVal;
-        int keyVal;
-        char keyChar;
-        char encryptedVal;
-        String returnText ="";
 
-        for (int i = 0; i < cryptText.length(); i++){
-            textChar = intToChar(cryptText.charAt(i));
-            keyChar = onetimepad.charAt(i);
-            keyVal = Character.getNumericValue(keyChar);
-            cipherVal = textChar - keyVal;
-            encryptedVal = intToChar(cipherVal);
-            returnText += encryptedVal;
-        }
-        return returnText; */
         return cryptText;
 	}
 
@@ -81,7 +69,7 @@ public class onetimepad {
                 System.out.println("Enter text to encrypt...");
                 toEncrypt = sc.next();
                 toEncrypt = sc.nextLine();
-                toDecrypt = encipher(toEncrypt, key);
+                toDecrypt = xor(toEncrypt, key);
                 System.out.println(toDecrypt);
                 System.out.println(toEncrypt);
             }
