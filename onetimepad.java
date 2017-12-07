ed8d21bc3527f66865530610e8e3670fbdb7389c
Eric Kimball <eric-k93@live.com>
2017-12-06 23:02:27 -0800

    done

--- onetimepad.java
+++ onetimepad.java
@@ -1,90 +1,79 @@
 import java.util.Scanner;
 import java.lang.Character;
+
 public class onetimepad {
-
     public static int charToInt(char l) {
-		int ascii = (int) l;
-		return ascii;
-    }
 
+		return(int)l;
+    }
     public static char intToChar(int i) {
-		char result = (char)i;
-		return result;
+    	return(char)i;
     }
+    public static String encrypt(String original, String key) {
 
-
-	public static String encipher(String string, String key) {
-        final byte[] text = string.getBytes("UTF-8");
-        final byte[] keyByte = key.getBytes("UTF-8");
-        final byte[] encoded = new byte[text.length];
-
-		for (int i = 0; i < text.length; i++) {
-			encoded[i] = (byte) (text[i] ^ keyByte[i]);
+        char returnChar, selectedChar;
+    	String encipheredText = "";
+        int originalText, newText, shifter;
+        
+    	for (int i = 0; i < original.length(); i++){
+    		originalText =  charToInt(original.charAt(i));
+    		selectedChar = key.charAt(i);
+    		shifter = Character.getNumericValue(selectedChar);
+    		newText = originalText + shifter;
+    		returnChar = intToChar(newText);
+    		encipheredText += String.valueOf(returnChar);
+    	}
+    	return encipheredText;
+    }
+	public static String decrypt(String encryptedText, String key) {
+		int shifter, originalText, newText;
+    	char selectedChar, returnChar;
+    	String encipheredText = "";
+    	for (int i = 0; i < encryptedText.length(); i++){
+    		originalText =  charToInt(encryptedText.charAt(i));
+    		selectedChar = key.charAt(i);
+    		shifter = Character.getNumericValue(selectedChar);
+    		newText = originalText - shifter;
+    		returnChar = intToChar(newText);
+    		encipheredText += String.valueOf(returnChar);
+    	}
+		return encipheredText;
+	}
+    public static void main(String[] args) {
+		boolean quit = false;
+		String key = "";
+		String to_encrypt = "";
+		String to_decrypt = "";
+		int input;
+		String ciphertext = "";
+		while (quit != true){
+			System.out.println(" 1. Key \n 2. Encrypt \n 3. Decrpyt \n 4. Quit");
+			Scanner arg = new Scanner(System.in);
+			input = arg.nextInt();
+			if (input == 1){
+				System.out.println("Key: ");
+				key = arg.next();
+				key += arg.nextLine();
+			}
+			else if (input == 2){
+				System.out.println("Enter text to be encrypted:");
+				to_encrypt = arg.next();
+				to_encrypt += arg.nextLine();
+				if (key.length() != to_encrypt.length()){
+                    throw new IllegalArgumentException("Non-Matching key and text length.");
+                };
+                to_decrypt = encrypt(to_encrypt, key);
+                System.out.println(to_decrypt);
+			}
+			else if (input == 3){
+				ciphertext = decrypt(to_decrypt, key);
+				System.out.println(ciphertext);
+			}
+			else if (input == 4){
+				quit = true;
+			}
+			else System.out.println("Invalid.");
 		}
-        String encipheredString = new String(encoded, "UTF-8");
-        return encipheredString;
-	}
-
-	public static String decipher(String string, String key) {
-        final byte[] text = string.getBytes("UTF-8");
-        final byte[] keyByte = key.getBytes("UTF-8");
-        final byte[] deciphered = new byte[text.length];
-
-        for (int i = 0; i < text.length; i ++) {
-            deciphered = (byte) (text[i] ^ keyByte[i]);
-        }
-        String decipheredString = new String(deciphered, "UTF-8");
-        return decipheredString;
-	}
-
-
-    public static void main(String[] args) throws IllegalArgumentException{
-        boolean quit = false;
-        String key = "";
-        String toEncrypt = "";
-        String toDecrypt = "";
-        int input;
-        String decrypted = "";
-
-
-        while (quit != true) {
-            System.out.println("________________Menu________________ \n 1. Encryption Key \n 2. Encrypt \n 3. Decrypt \n 4. Quit");
-            Scanner sc = new Scanner(System.in);
-            input = sc.nextInt();
-
-
-            if (input == 1){
-                System.out.println("Enter encryption key...");
-                key = sc.next();
-                key += sc.nextLine();
-                System.out.println("Your key is..." + key);
-            }
-
-            else if (input == 2) {
-                System.out.println("Enter text to encrypt...");
-                toEncrypt = sc.next();
-                toEncrypt += sc.nextLine();
-                System.out.println("char " + toEncrypt);
-                toDecrypt = encipher(toEncrypt, key);
-                System.out.println("Encrypted:" + toDecrypt);
-            }
-
-            else if (input == 3) {
-                System.out.println("Decryption...");
-                //toDecrypt = sc.next();
-                //toDecrypt += sc.nextLine();
-                decrypted = decipher(toDecrypt, key);
-                System.out.println(decrypted);
-            }
-
-            else if (input == 4) {
-                quit = true;
-                sc.close();
-            }
-
-            else System.out.println("Invalid input");
-        }
-        //String ciphertext = encipher("this is my message", "this is my secret key");
-		//System.out.print(ciphertext);
     }
+
 }
